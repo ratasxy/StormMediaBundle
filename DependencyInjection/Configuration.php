11f4d693739ec2363f8c346c276ef46dcf86b1c9
Ernesto Jose Vargas Paz <ejosvp@gmail.com>
2011-12-30 17:45:33 -0500

    repair configuration root node

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -16,7 +16,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('storm.media');
+        $rootNode = $treeBuilder->root('storm_media');
 
         $rootNode
             ->children()
